7e628b1fe4411e722e788087dc13900713bed1cd
smuyyh <352091626@qq.com>
2016-11-11 16:07:47 +0800

    调整首页菜单图标

--- app/src/main/java/com/yuyh/sprintnba/ui/Interactor/impl/HomeInteractorImpl.java
+++ app/src/main/java/com/yuyh/sprintnba/ui/Interactor/impl/HomeInteractorImpl.java
@@ -34,12 +34,12 @@ public class HomeInteractorImpl implements HomeInteractor {
     @Override
     public List<NavigationEntity> getNavigationList(Context context) {
         List<NavigationEntity> navigationEntities = new ArrayList<NavigationEntity>() {{
-            add(new NavigationEntity(R.drawable.ic_news, "NBA头条"));
-            add(new NavigationEntity(R.drawable.ic_video, "赛事直播"));
-            add(new NavigationEntity(R.drawable.ic_format, "球队战绩"));
-            add(new NavigationEntity(R.drawable.ic_format, "数据排行"));
-            add(new NavigationEntity(R.drawable.ic_favorite, "虎扑专区"));
-            add(new NavigationEntity(R.drawable.ic_other, "其他"));
+            add(new NavigationEntity(R.drawable.ic_top_news, "NBA头条"));
+            add(new NavigationEntity(R.drawable.ic_videos, "赛事直播"));
+            add(new NavigationEntity(R.drawable.ic_score, "球队战绩"));
+            add(new NavigationEntity(R.drawable.ic_sort, "数据排行"));
+            add(new NavigationEntity(R.drawable.ic_hupu, "虎扑专区"));
+            add(new NavigationEntity(R.drawable.ic_others, "其他"));
         }};
         return navigationEntities;
     }
