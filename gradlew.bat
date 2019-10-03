df2658f826db61f456ca85e6ad10051900e42fcb
tonyspicervovin <42811702+tonyspicervovin@users.noreply.github.com>
2019-10-03 17:03:15 -0500

    added a change

--- gradlew.bat
+++ gradlew.bat
@@ -38,6 +38,7 @@ set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 if exist "%JAVA_EXE%" goto init
 
 echo.
+a change
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
 echo.
 echo Please set the JAVA_HOME variable in your environment to match the
